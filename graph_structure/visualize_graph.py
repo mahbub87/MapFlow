3340109e2343e6bd372d60afdc876b37ccaa1aa1
Mahbub Shafin <mahbub86@my.yorku.ca>
2024-09-13 18:20:14 -0400

    Commit

--- graph_structure/visualize_graph.py
+++ graph_structure/visualize_graph.py
@@ -1,23 +1,30 @@
 import matplotlib.pyplot as plt
 
+
 def visualize_and_save_graph(G, output_file):
     plt.figure(figsize=(50, 50))
 
-    # Plot edges
+    # Set the background color to black
+    plt.gca().set_facecolor('black')
+
+    # Plot edges with cyan color
     for node in G.nodes.values():
         for edge in node.edges:
             x_values = [edge.node1.longitude, edge.node2.longitude]
             y_values = [edge.node1.latitude, edge.node2.latitude]
-            plt.plot(x_values, y_values, 'b-', linewidth=0.5)
+            plt.plot(x_values, y_values, 'c-', linewidth=0.5)
 
-    # Plot nodes
+    # Plot nodes with red color
     for node in G.nodes.values():
-        plt.plot(node.longitude, node.latitude, 'ro', markersize=0.75)
+        plt.plot(node.longitude, node.latitude, 'ro', markersize=0)
 
-    plt.title('City Graph')
-    plt.xlabel('Longitude')
-    plt.ylabel('Latitude')
-    plt.grid(True)
+    # Remove title, xlabel, and ylabel
+    plt.title('')
+    plt.xlabel('')
+    plt.ylabel('')
+
+    # Remove grid if desired
+    plt.grid(False)
 
     # Save the plot to a file
     plt.savefig(output_file, bbox_inches='tight')
--- main.py
+++ main.py
@@ -1,49 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import os
 
-from data import osm_data
-from graph_structure import visualize_graph
 from graph_structure.Graph import Graph
-from graph_structure.osm_to_graph import create_graph_from_shapefiles
 
 app = Flask(__name__)
-
 
 @app.route('/get_city', methods=['POST'])
 def get_city():
     city = request.json['city']
-    match(city):
-        case 'Rome':
-            country = 'Italy'
-        case 'Chicago':
-            country = 'United States'
-        case 'Cairo':
-            country = 'Egypt'
-        case 'Berlin':
-            country = 'Germany'
-        case 'Paris':
-            country = 'France'
-        case 'Dhaka Metropolitan':
-            country = 'Bangladesh'
 
-    #osm_data.get_osm_data(city, country)
+
     print(city)
-    #g = create_graph_from_shapefiles(city)
+
     directory = r"C:\Users\shafi\PycharmProjects\PathFinder\data" + "\\"
     city_directory = os.path.join(directory, city)
-    file_name = 'graph.json'
-    file_path = os.path.join(city_directory, file_name)
-    #g.save_to_file(file_path)
-    # visualize_graph.visualize_and_save_graph(loaded_graph, 'graph.png')
-
+    file_path = os.path.join(city_directory, 'graph.json')
     loaded_graph = Graph.load_from_file(file_path)
 
-    return country
+    graph_data = loaded_graph.to_dict()
+    return jsonify(graph_data)  # Return the graph data as JSON
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
